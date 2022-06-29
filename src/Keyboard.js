9e3aa38b3fedceffa1de3bff12b6d7efa5af926e
Anubhav Mehra <anubhav.mehra@hotmail.com>
2022-06-28 23:27:16 -0700

    changed bck key

--- src/Keyboard.js
+++ src/Keyboard.js
@@ -1,8 +1,8 @@
 export default function Keyboard(props) {
     const virtInput = props.virtInput;
-    const keys = [['q','w','e','r','t','y','u','i','o' ,'p' ,'BCK'],
+    const keys = [['q','w','e','r','t','y','u','i','o' ,'p'],
                   ['a', 's','d', 'f', 'g', 'h', 'j', 'k', 'l'],
-                  ['z','x','c', 'v','b','n','m','ENT']];
+                  ['BCK','z','x','c', 'v','b','n','m','ENT']];
     const firstRow = keys[0].map( key => {
         return (
             <button id={key} key={key} className = "virtKey" onClick={() => {virtInput(key)} }>{key.toUpperCase()}</button>
